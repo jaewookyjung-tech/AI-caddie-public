5c78e4d8d499839ec0a0460dc6e9a23e2a422c5c
jaewookyjung-tech <jaewookyjung@gmail.com>
2026-06-11 23:44:19 +0100

    Add src files

---
+++ main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CaddieApp from "./CaddieApp.jsx";
+createRoot(document.getElementById("root")).render(<CaddieApp />);
